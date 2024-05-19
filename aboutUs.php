fa982f1510458eaaf80d57633fae66214f8f1648
Rolando Morales Jr <rolando.moralesjr21@gmail.com>
2024-05-19 04:49:25 -0500

    About Us Page initial commit

---
+++ aboutUs.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>About Us</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    
+    <nav class="navbar navbar-custom nav-style" >
+        <div class="container-fluid width-format">
+            <div class="navbar-header">
+                <a href="#" class="navbar-brand roboto-bold store-name">SampleName</a>
+            </div>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a class="light-black" href="index.php">Home</a></li>
+                <li><a class="light-black" href="products.php">Products</a></li>
+                <li><a class="light-black" href="cart.php">Cart</a></li>
+                <li><a class="light-black" href="aboutUs.php">About Us</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <p>About us</p>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,8 +5,6 @@
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- <link rel="stylesheet" href="styles.css"> -->
     
     <style>
 
