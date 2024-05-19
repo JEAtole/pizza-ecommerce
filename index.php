100416d2f20841f14c28184fe7cb12ae9fc8c3c6
Rolando Morales Jr <rolando.moralesjr21@gmail.com>
2024-05-19 02:35:07 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>Portfolio</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+
+</head>
+
+<body data-spy="scroll" data-target=".navbar" >
+    Main page
+</body>
+
+</html>
